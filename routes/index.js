8ffe9f877457d26921ec4ccd56979a76c36ca8df
Justin Samuels <blizm87@gmail.com>
2017-03-17 00:04:30 -0700

    changed index page trivia request

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const request = require('request');
+const entities = require('html-entities').AllHtmlEntities;
 const router = express.Router();
 const title = 'PoliView Entertainment';
 
 router.get('/', (req, res, next) => {
-  console.log('I am rendering the home page');
-  res.render('index', {title: title});
+  const url = req.query.trivQuery;
+  console.log('I am the trivia request: ' + url)
+  request.get(url, (err, response, body) => {
+    const preData = JSON.parse(body).results[0];
+    var data = entities.decode(preData);
+    res.send(data);
+  });
 });
 
 module.exports = router;
