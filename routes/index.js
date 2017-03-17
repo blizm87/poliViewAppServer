254da4ee0ea07387c78701ac0559229a7e3d9e3c
Justin Samuels <blizm87@gmail.com>
2017-03-17 16:07:00 -0700

    added quote route to retrieve quote from api

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const request = require('request');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/trivGame', (req, res, next) => {
   console.log('I am the category: ' + req.query.category)
   let url = `https://www.opentdb.com/api.php?amount=1&category=${req.query.category}&difficulty=medium&type=multiple`
   console.log('I am the url: ' + url)
   request.get(url, (err, response, body) => {
     const preData = JSON.parse(body).results[0];
     res.json({data: preData});
+  });
+});
+
+router.get('/quote', (req, res, next) => {
+  let url = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+  request.get(url, (err, response, body) => {
+    const data = JSON.parse(body).results[0];
+    res.json({data: data});
   });
 });
 
