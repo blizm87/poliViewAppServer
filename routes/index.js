ed5bdc0bb4c8f35a369621c4d7976ea8f5715fda
Justin Samuels <blizm87@gmail.com>
2017-03-17 00:23:09 -0700

    second attempt

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const request = require('request');
-// const entities = require('html-entities').AllHtmlEntities;
+const entities = require('html-entities').AllHtmlEntities;
 const router = express.Router();
 const title = 'PoliView Entertainment';
 
 router.get('/', (req, res, next) => {
   const url = req.query.trivQuery;
   console.log('I am the trivia request: ' + url)
-  // request.get(url, (err, response, body) => {
-  //   const preData = JSON.parse(body).results[0];
-  //   var data = entities.decode(preData);
-  //   res.send(data);
-  // });
-  res.json({data: 'I am working'});
+  request.get(url, (err, response, body) => {
+    const preData = JSON.parse(body).results[0];
+    var data = entities.decode(preData);
+    res.json({data: data});
+  });
 });
 
 module.exports = router;
