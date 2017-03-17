c42a1c7013136bbbbc757625f62323618da6cca6
Justin Samuels <blizm87@gmail.com>
2017-03-17 14:44:37 -0700

    trying to find why url becomes truncated

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const request = require('request');
-// const entities = require('html-entities').AllHtmlEntities;
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const url = req.query.trivQuery;
   console.log('I am the trivia request: ' + url)
+  console.log('I am the second request: ' + url)
   request.get(url, (err, response, body) => {
     const preData = JSON.parse(body).results[0];
-    // var data = entities.decode(preData);
     console.log('I am the trivia data: ' + preData)
     res.json({data: preData});
   });
