3be191a480a65be034a14514deeeb93b24bc6404
Justin Samuels <blizm87@gmail.com>
2017-03-17 00:10:03 -0700

    testing error in triv query

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ const title = 'PoliView Entertainment';
 router.get('/', (req, res, next) => {
   const url = req.query.trivQuery;
   console.log('I am the trivia request: ' + url)
-  request.get(url, (err, response, body) => {
-    const preData = JSON.parse(body).results[0];
-    var data = entities.decode(preData);
-    res.send(data);
-  });
+  // request.get(url, (err, response, body) => {
+  //   const preData = JSON.parse(body).results[0];
+  //   var data = entities.decode(preData);
+  //   res.send(data);
+  // });
 });
 
 module.exports = router;
