d0cd5cbfa442b175f369aa0336282f30d2dbe956
Justin Samuels <blizm87@gmail.com>
2017-03-17 17:55:24 -0700

    replaced html dependancy

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,15 @@ router.get('/', (req, res, next) => {
   res.json({data: "I worked!!!"})
 })
 
-// router.get('/trivGame', (req, res, next) => {
-//   console.log('I am the category: ' + req.query.category)
-//   let url = `https://www.opentdb.com/api.php?amount=1&category=${req.query.category}&difficulty=medium&type=multiple`
-//   console.log('I am the url: ' + url)
-//   request.get(url, (err, response, body) => {
-//     const preData = JSON.parse(body).results[0];
-//     res.json({data: preData});
-//   });
-// });
+router.get('/trivGame', (req, res, next) => {
+  console.log('I am the category: ' + req.query.category)
+  let url = `https://www.opentdb.com/api.php?amount=1&category=${req.query.category}&difficulty=medium&type=multiple`
+  console.log('I am the url: ' + url)
+  request.get(url, (err, response, body) => {
+    const preData = JSON.parse(body).results[0];
+    res.json({data: preData});
+  });
+});
 
 // router.get('/quote', (req, res, next) => {
 //   let url = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
